eb5ded5a06eceb7fecb77789f30c71b48391b99a
Augusto Colombelli Alessio <augustocolombelli@gmail.com>
2018-10-26 19:08:28 -0300

    some refactoring in the chart use case

--- src/br/com/project/bean/charts/ExchangeVariationBean.java
+++ src/br/com/project/bean/charts/ExchangeVariationBean.java
@@ -7,12 +7,10 @@ import java.util.List;
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
 
-import org.jboss.resteasy.util.DateUtil;
 import org.primefaces.model.chart.AxisType;
 import org.primefaces.model.chart.DateAxis;
 import org.primefaces.model.chart.LineChartModel;
 import org.primefaces.model.chart.LineChartSeries;
-import org.primefaces.util.DateUtils;
 
 import br.com.project.model.Exchange;
 import br.com.project.service.ExchangeService;
@@ -20,16 +18,21 @@ import br.com.project.service.ExchangeService;
 @Model
 public class ExchangeVariationBean implements Serializable {
 	private static final long serialVersionUID = 1L;
-	private LineChartModel dataModel;
+	
 
 	@Inject
 	private ExchangeService exchangeService;
 	
+//	public LineChartModel getDataModel() {
+//		generateChart();
+//		return dataModel;
+//	}
 	
-	private void generateChart() {
+	public LineChartModel getLineChartModel() {
 		List<Exchange> exchanges = exchangeService.getAll();
-		List<ExchangeVariationSerie> exchangeSeries = generateDataFromChart(exchanges);
+		List<ExchangeVariationSerie> exchangeSeries = generateDataToChart(exchanges);
 		
+		LineChartModel dataModel;
 		dataModel = new LineChartModel();
 
 		for (ExchangeVariationSerie exchangeSerie : exchangeSeries) {
@@ -53,9 +56,10 @@ public class ExchangeVariationBean implements Serializable {
 
 		dataModel.getAxes().put(AxisType.X, axis);
 		
+		return dataModel;
 	}
 	
-	private List<ExchangeVariationSerie> generateDataFromChart(List<Exchange> exchanges){
+	private List<ExchangeVariationSerie> generateDataToChart(List<Exchange> exchanges){
 		List<ExchangeVariationSerie> exchangeSeries = new ArrayList<ExchangeVariationSerie>();
 		
 		for(Exchange exchange : exchanges) {
@@ -91,50 +95,6 @@ public class ExchangeVariationBean implements Serializable {
 		return null;
 	}
 	
-	public LineChartModel getDataModel() {
-		generateChart();
-		return dataModel;
-	}
-//
-//	private void createDateModel() {
-//		dataModel = new LineChartModel();
-//
-//		LineChartSeries serie1 = new LineChartSeries();
-//		serie1.setLabel("Dolar");
-//		serie1.set("2014-01-01", 10.2);
-//		serie1.set("2014-01-06", 2.4);
-//		serie1.set("2014-01-12", 4.4);
-//		serie1.set("2014-01-18", 34.2);
-//
-//		LineChartSeries serie2 = new LineChartSeries();
-//		serie2.setLabel("Real");
-//		serie2.set("2014-01-01", 61.2);
-//		serie2.set("2014-01-06", 32.4);
-//		serie2.set("2014-01-12", 15.4);
-//		serie2.set("2014-01-18", 24.2);
-//
-//		LineChartSeries serie3 = new LineChartSeries();
-//		serie3.setLabel("Guarani");
-//		serie3.set("2014-01-01", 5.2);
-//		serie3.set("2014-01-06", 57.4);
-//		serie3.set("2014-01-12", 72.4);
-//		serie3.set("2014-01-18", 7.2);
-//
-//		dataModel.addSeries(serie1);
-//		dataModel.addSeries(serie2);
-//		dataModel.addSeries(serie3);
-//
-//		dataModel.setTitle("Title...");
-//		dataModel.setZoom(true);
-//		dataModel.setLegendPosition("e");
-//		dataModel.getAxis(AxisType.Y).setLabel("Values");
-//
-//		DateAxis axis = new DateAxis("Dates");
-//		axis.setTickAngle(-50);
-//		axis.setMax("2014-02-01");
-//		axis.setTickFormat("%b %#d, %y");
-//
-//		dataModel.getAxes().put(AxisType.X, axis);
-//	}
+
 
 }
